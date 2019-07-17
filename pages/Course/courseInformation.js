82df571b3e1299c8fc9a2732e87ff7caad38428b
PalatipReal <sungsilver3@gmail.com>
2019-07-17 18:06:13 +0700

    Add CourseRegis Page

--- pages/Course/courseInformation.js
+++ pages/Course/courseInformation.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Form from '../../Component/form.js'
 import Head from '../../Component/head.js';
+import Router from 'next/router';
 import './CourseStyle.css';
-import { Nav, NavItem, NavLink,Row,Col,Container } from 'reactstrap';
+import { Nav, NavItem, NavLink,Row,Col,Container,Button } from 'reactstrap';
 import HeadTitle from "../../Component/DashBoard/HeaderTitle"
 export default function courseInformation(props) {
+
+  function HandleClick () {
+    Router.push({
+      pathname: '/Course/courseregisform',
+      query: {
+          name: props.url.query.name
+      },
+  })
+  }
     return (
         <div>
             <Head title="Education - Project Lab" />
@@ -12,6 +22,7 @@ export default function courseInformation(props) {
             <br/>
             <HeadTitle Name={"Robotics Courses (in House)"}/>
             <br/>
+{/* ####################### Banner Start Here.. ####################### */}
             <header style={{
                 height: "100vh",
                 minHeight: "500px",
@@ -27,6 +38,9 @@ export default function courseInformation(props) {
                 </div>
               </div>
             </header>
+{/* ####################### Banner End Here.. ####################### */}
+
+{/* ####################### Main-Content Start Here.. ####################### */}
         <Container>
         <Nav>
           <NavItem>
@@ -40,29 +54,68 @@ export default function courseInformation(props) {
         </div>
         <Row style={{marginTop:"20px"}}>
             <Col>
-            <h2 >{props.url.query.name} COURSE</h2>
+            <h2 style={{fontWeight:"bold"}}>{props.url.query.name} COURSE</h2>
             <Row>
                 <Col sm="10">
-                    <h5>Ace:4-7 </h5>  <p>Computational Thinking :: Programming :: Innovative Thinking</p>
+                    <h5 style={{color:"LightSeaGreen",fontWeight:"bold"}}>Ace : 4 to 7 </h5>  <p>Computational Thinking :: Programming :: Innovative Thinking</p>
                 </Col>
             </Row>
             </Col>
             <Col > 
-               <button>Sign up Now</button>
+               <Button onClick={HandleClick} color="primary">Sign up Now</Button>
             </Col>
         </Row>
-        CLASS SIZE : 4 STUDENTS
-        DURATION : 8 HOURS ( 2 HOURS / CLASS )<br/>
-        FEE / STUDENT : 7,200 THB<br/>
-        COURSE DETAIL : Kids code virtual 3D robots in an engaging online environment, making this an easy, ecient and
-        exciting way to learn both coding and robotics!<br/>
-        CLASS SCHEDULE : <br/>
-        DAY :<br/>
-        TIME : <br/>
-        COURSE START:<br/>
-        Mojobot คืออะไร<br/>
+
+        <div className="d-flex justify-content-start">
+            <h6 style={{color:"LightSeaGreen",fontWeight:"900"}}>CLASS SIZE : </h6>
+            &nbsp;
+            &nbsp; 
+            <h6 style={{fontWeight:"bolder"}}>4 STUDENTS</h6>
+        </div>
         
+        <div className="d-flex justify-content-start">
+            <h6 style={{color:"LightSeaGreen",fontWeight:"900"}}>DURATION : </h6>
+            &nbsp;
+            &nbsp;
+            &nbsp;  
+            <h6 style={{fontWeight:"bolder"}}>8 HOURS ( 2 HOURS / CLASS )</h6>
+        </div> 
+
+        <div className="d-flex justify-content-start">
+            <h6 style={{color:"LightSeaGreen",fontWeight:"900"}}>FEE / STUDENT : </h6>
+            &nbsp;
+            &nbsp; 
+            <h6 style={{fontWeight:"bolder"}}>7,200 THB</h6>
+        </div> 
+
+        <div className="d-flex justify-content-start">
+            <h6 style={{color:"LightSeaGreen",fontWeight:"900"}}>COURSE DETAIL : </h6>
+            &nbsp;
+            &nbsp; 
+            <h6 style={{fontWeight:"bolder"}}>Kids code virtual 3D robots in an engaging online environment, making this an easy, ecient and
+            exciting way to learn both coding and robotics!</h6>
+        </div> 
+        <br/>
+        <br/>
+
+        <h6 style={{color:"LightSeaGreen",fontWeight:"900"}}>CLASS SCHEDULE : FIXED SCHEDULE*</h6>
+        <div className="d-flex justify-content-start">
+          <h6 style={{fontWeight:"500"}}>DAY :</h6>
+          <h6>SUNDAY</h6>
+        </div>
+
+        <h6 style={{fontWeight:"500"}}>TIME :</h6>
+        <h6 style={{fontWeight:"500"}}>COURSE START:</h6>
+{/* #######################  Main-Content End Here.. #######################  */}   
+
+{/* #######################  Text Editor Display Start Here... #######################  */}
+<h5 className="text-center">Text Editor Display Here....</h5>
+{/* #######################  Text Editor Display End Here... #######################  */}
         </Container>
+        <div className="text-center">
+          <Button  color="primary" onClick={HandleClick}>sign up now</Button>
+        </div>
+        
         </Form>
         </div>
     )
